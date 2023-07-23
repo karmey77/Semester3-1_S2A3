1aeb3c541ef2c97aa879523150e88d7c058a6292
Yi-Hsiu Lee <tmrmds.yhl@gmail.com>
2023-07-23 14:19:31 +0800

    add feat: schema

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const routes = require('./routes') // 引用路由器
-// require('./config/mongoose')
+require('./config/mongoose')
 
 const app = express()
 const PORT = process.env.PORT || 3000
---
+++ models/records.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    id: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    userId: {  // 加入關聯設定
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        index: true,
+        required: true
+    },
+    categoryId: {  // 加入關聯設定
+        type: Schema.Types.ObjectId,
+        ref: 'Category',
+        index: true,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Record', userSchema)
